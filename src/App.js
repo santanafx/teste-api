2d89a1e6c88923c593e3d96a2f7021a14d43d8d9
Lucas Santana Figueiredo <santanafx@hotmail.com>
2023-06-04 09:02:50 -0300

    ...

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-
+import React from 'react'
 import './App.css';
 
 function App() {
+  const [estado, setEstado] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch('http://localhost:3000/dataBase', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+
+
+    }).then((resp) => resp.json()).then(data => { setEstado(data) })
+    console.log('teste')
+  }, [])
+
   return (
     <div >
-      teste
     </div>
   );
 }
